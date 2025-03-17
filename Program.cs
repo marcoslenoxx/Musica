fa57dd9e1a2351ff9e11f7ba17d9f58f5d5d164a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-17 20:24:13 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("We Found Love");
+Console.ResetColor();
+Console.WriteLine(@"
+Yellow diamonds in the light
+Now we re standing side by side
+As your shadow crosses mine
+What it takes to come alive
+
+It s the way I m feeling I just can t deny
+But I ve gotta let it go");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"
+We found love in a hopeless place
+We found love in a hopeless place
+We found love in a hopeless place
+We found love in a hopeless place");
+Console.ResetColor();
+Console.WriteLine(@"
+Shine a light through an open door
+Love and life I will divide
+Turn away  cause I need you more
+Feel the heartbeat in my mind
+
+It s the way I m feeling I just can t deny
+But I ve gotta let it go");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"
+We found love in a hopeless place
+We found love in a hopeless place
+We found love in a hopeless place
+We found love in a hopeless place");
+Console.ResetColor();
+Console.WriteLine(@"
+Yellow diamonds in the light
+Now we re standing side by side
+As your shadow crosses mine mine, mine, mine");
